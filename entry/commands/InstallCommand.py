9652910f6a9903c82b9d3f899483c5ccbe6939bb
Joseph Mancuso <idmann509@gmail.com>
2018-03-23 11:06:18 -0400

    added commands to enhance installation

--- entry/commands/InstallCommand.py
+++ entry/commands/InstallCommand.py
@@ -1,20 +1,26 @@
 import os
+import subprocess
+from cleo import Command
 from masonite.packages import create_controller, append_web_routes
 
 
 package_directory = os.path.dirname(os.path.realpath(__file__))
 
-class EntryInstallCommand:
+class InstallCommand(Command):
+    """
+    Installs needed controllers and routes into a Masonite project
 
-    def execute(self):
+    entry:install
+    """
+
+    def handle(self):
         create_controller(
             os.path.join(package_directory,
                          '../entry_snippets/controllers/PasswordGrantController.py'),
             to='app/http/controllers/Entry/Api'
         )
-
         append_web_routes(
             os.path.join(package_directory,
                          '../entry_snippets/routes/EntryRoutes.py')
         )
-
+        subprocess.call(['craft', 'migrate'])
