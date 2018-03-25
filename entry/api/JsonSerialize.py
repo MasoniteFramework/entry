7cbb6fecb757db44e522cec8b1c7be851f342f32
Joseph Mancuso <idmann509@gmail.com>
2018-03-25 16:18:57 -0400

    restructured resources and json serializing

--- entry/api/JsonSerialize.py
+++ entry/api/JsonSerialize.py
@@ -1,117 +1,13 @@
 import json
-import re
-import bcrypt
+
+
 
 class JsonSerialize:
-    model = None
-    request = None
-    url = None
-    relationships = None
-    exclude_relationship_fields = []
 
-    def serialize(self):
-        for item in list(self.model().__dict__):
-            if item.startswith('_'):
-                self.model().__dict__.pop(item)
-        try:
-            if self.request.environ['REQUEST_METHOD'] == 'POST' and 'create' in self.methods:
-                return self.create()
-
-            if self.request.environ['REQUEST_METHOD'] == 'GET' and 'read' in self.methods:
-                return self.read()
-
-            if self.request.environ['REQUEST_METHOD'] == 'PUT' and 'update' in self.methods:
-                return self.update()
-            
-            if self.request.environ['REQUEST_METHOD'] == 'DELETE' and 'delete' in self.methods:
-                return self.delete()
-        except Exception as e:
-            return json.dumps(
-                {
-                    'error': str(e)
-                }
-            )
-
-        return json.dumps(
-            {
-                'Error': 'Invalid URI: {0} {1}. This route does not exist for this endpoint.'.format(
-                self.request.environ['REQUEST_METHOD'], self.request.path)
-            }
-        )
-
-    def create(self):
-        if '{0}s'.format(self.url) == self.request.path:
-            # if POST /api/users
-            proxy = self.model()
-            for field in self.request.all():
-                # If the field is a password, hash it
-                if field == 'password':
-                    password = bcrypt.hashpw(
-                        bytes(self.request.input('password'), 'utf-8'), bcrypt.gensalt()
-                    )
-                    setattr(proxy, field, password)
-                else:
-                    setattr(proxy, field, self.request.input(field))
-            proxy.save()
-
-            return proxy.to_json()
-
-    def _get_relationships(self, model):
-        # Get relationships
-        for relationship in self.relationships:
-            if hasattr(model, relationship):
-                attr = getattr(model, relationship)
-                if attr and relationship in self.exclude_relationship_fields:
-                    attr.__hidden__ = self.exclude_relationship_fields[relationship]
-
-    def read(self):
-        matchregex = re.compile(r"^\/\w+\/\w+\/(\d+)")
-        match_url = matchregex.match(self.request.path)
-
-        # Get the plural of the url
-        if '{0}s'.format(self.url) == self.request.path:
-            # if GET /api/users
-            models = self.model().all()
-            if self.relationships:
-                for model in models:
-                    self._get_relationships(model)
-                        
-            return models.to_json()
-        elif match_url:
-            # if GET /api/user/1
-            record = self.model().find(match_url.group(1))
-            if record:
-                if self.relationships:
-                    self._get_relationships(record)
-                return record.to_json()
-            else:
-                return json.dumps({'Error': 'Record Not Found'})
-
-        return json.dumps({'Error': 'Invalid URI: {0}. Did you mean {0}s?'.format(self.url)})
-
-    def update(self):
-        # if PUT /api/user/1
-        matchregex = re.compile(r"^\/\w+\/\w+\/(\d+)")
-        match_url = matchregex.match(self.request.path)
-
-        proxy = self.model.find(match_url.group(1))
-        if not proxy:
-            return json.dumps({'Error': 'Record Not Found'})
-
-        for field in self.request.all():
-            setattr(proxy, field, self.request.input(field))
-        proxy.save()
-        proxy = self.model.find(match_url.group(1))
-        return proxy.to_json()
-
-    def delete(self):
-        # if DELETE /api/user/1
-        matchregex = re.compile(r"^\/\w+\/\w+\/(\d+)")
-        match_url = matchregex.match(self.request.path)
-
-        get = self.model.find(match_url.group(1))
-        if get:
-            get.delete()
-            return get.to_json()
+    def serialize(self, data):
+        if isinstance(data, dict):
+            return json.dumps(data)
         else:
-            return json.dumps({'Error': 'Record Not Found'})
+            return data.to_json()
+        
+
--- entry/api/Resource.py
+++ entry/api/Resource.py
@@ -1,4 +1,6 @@
 import json
+import bcrypt
+import re
 from entry.api.exceptions import (
     ApiNotAuthenticated,
     NoApiTokenFound,
@@ -12,9 +14,12 @@ from entry.api.exceptions import (
 class Resource:
 
     exclude = []
-    url = None
-    model = None
+    exclude_relationship_fields = []
     methods = ['create', 'read', 'update', 'delete']
+    model = None
+    relationships = None
+    request = None
+    url = None
 
     def __init__(self):
         self.model.__hidden__ = self.exclude
@@ -34,13 +39,39 @@ class Resource:
                 return json.dumps({'error': 'Authentication token not found'})
             except PermissionScopeDenied:
                 return json.dumps({'error': 'Incorrect permission scope'})
+
+        # Run rate limiting if one exists
         if hasattr(self, 'limit'):
             try:
                 self.limit()
             except RateLimitReached:
                 return json.dumps({'error': 'Rate limit of {0} calls every {1} {2} reached'.format(self.rate_limit[0], self.rate_limit[1], self.rate_limit[2])})
 
-        return self.serialize()
+        for item in list(self.model().__dict__):
+            if item.startswith('_'):
+                self.model().__dict__.pop(item)
+
+        try:
+            if self.request.environ['REQUEST_METHOD'] == 'POST' and 'create' in self.methods:
+                return self.serialize(self.create())
+
+            if self.request.environ['REQUEST_METHOD'] == 'GET' and 'read' in self.methods:
+                return self.serialize(self.read())
+
+            if self.request.environ['REQUEST_METHOD'] == 'PUT' and 'update' in self.methods:
+                return self.serialize(self.update())
+
+            if self.request.environ['REQUEST_METHOD'] == 'DELETE' and 'delete' in self.methods:
+                return self.serialize(self.delete())
+        except Exception as e:
+            return self.serialize({'error': str(e)})
+
+        return self.serialize(
+            {
+                'Error': 'Invalid URI: {0} {1}. This route does not exist for this endpoint.'.format(
+                    self.request.environ['REQUEST_METHOD'], self.request.path)
+            }
+        )
     
     def load_request(self, request):
         self.request = request
@@ -49,3 +80,80 @@ class Resource:
     def load_container(self, container):
         self.container = container
         return self
+
+    def create(self):
+        if '{0}s'.format(self.url) == self.request.path:
+            # if POST /api/users
+            proxy = self.model()
+            for field in self.request.all():
+                # If the field is a password, hash it
+                if field == 'password':
+                    password = bcrypt.hashpw(
+                        bytes(self.request.input('password'),
+                              'utf-8'), bcrypt.gensalt()
+                    )
+                    setattr(proxy, field, password)
+                else:
+                    setattr(proxy, field, self.request.input(field))
+            proxy.save()
+
+            return proxy
+
+    def read(self):
+        matchregex = re.compile(r"^\/\w+\/\w+\/(\d+)")
+        match_url = matchregex.match(self.request.path)
+
+        # Get the plural of the url
+        if '{0}s'.format(self.url) == self.request.path:
+            # if GET /api/users
+            models = self.model().all()
+            if self.relationships:
+                for model in models:
+                    self._get_relationships(model)
+
+            return models
+        elif match_url:
+            record = self.model().find(match_url.group(1))
+            if record:
+                if self.relationships:
+                    self._get_relationships(record)
+                return record
+            else:
+                return {'Error': 'Record Not Found'}
+
+        return {'Error': 'Invalid URI: {0}. Did you mean {0}s?'.format(self.url)}
+
+    def update(self):
+        # if PUT /api/user/1
+        matchregex = re.compile(r"^\/\w+\/\w+\/(\d+)")
+        match_url = matchregex.match(self.request.path)
+
+        proxy = self.model.find(match_url.group(1))
+        if not proxy:
+            return {'Error': 'Record Not Found'}
+
+        for field in self.request.all():
+            setattr(proxy, field, self.request.input(field))
+        proxy.save()
+        proxy = self.model.find(match_url.group(1))
+        return proxy
+
+    def delete(self):
+        # if DELETE /api/user/1
+        matchregex = re.compile(r"^\/\w+\/\w+\/(\d+)")
+        match_url = matchregex.match(self.request.path)
+
+        get = self.model.find(match_url.group(1))
+        if get:
+            get.delete()
+            return get
+        else:
+            return {'Error': 'Record Not Found'}
+
+    def _get_relationships(self, model):
+        # Get relationships
+        for relationship in self.relationships:
+            if hasattr(model, relationship):
+                attr = getattr(model, relationship)
+                if attr and relationship in self.exclude_relationship_fields:
+                    attr.__hidden__ = self.exclude_relationship_fields[relationship]
