c7e633945d7872463447f5752c2f36f827d0fe9f
Joseph Mancuso <idmann509@gmail.com>
2018-03-29 22:37:19 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="masonite-entry",
-    version='0.0.2',
+    version='0.1.0',
     packages=['entry'],
     install_requires=[
-        'masonite',
+        'masonite>=1.5,<= 1.5.99',
     ],
     include_package_data=True,
 )
