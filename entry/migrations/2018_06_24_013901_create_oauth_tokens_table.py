dbc9e978c44f7e077eac919c248fe6db2271bbb9
Joseph Mancuso <idmann509@gmail.com>
2018-06-24 19:57:08 -0400

    fixed scopes to scope

--- entry/migrations/2018_06_24_013901_create_oauth_tokens_table.py
+++ entry/migrations/2018_06_24_013901_create_oauth_tokens_table.py
@@ -13,7 +13,7 @@ class CreateOauthTokensTable(Migration):
             table.string('token', 255).nullable()
             table.string('code', 255)
             table.string('refresh_token', 255).nullable()
-            table.string('scopes', 255).nullable()
+            table.string('scope', 255).nullable()
             table.datetime('expires_at').nullable()
             table.datetime('refresh_expires_at').nullable()
             table.timestamps()
